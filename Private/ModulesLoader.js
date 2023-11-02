52ef392b27dcb24310ffe4360bb05ba8e2ca712f
konbraphat51 <brightray48@zeus.eonet.ne.jp>
2023-11-02 10:22:52 +0900

    Improve: add script element

--- Private/ModulesLoader.js
+++ Private/ModulesLoader.js
@@ -2,5 +2,8 @@
 
 const HSSModules = ["Settings.js", "Graphics.js", "Utils.js", "Inputs.js", "Geometry.js", "Math.js", "HTML.js"]
 for (let i = 0; i < HSSModules.length; i++) {
-    document.write("<script src=\"HotSoupScript/Public/" + HSSModules[i] + "\"></script>")
+    //document.write("<script src=\"HotSoupScript/Public/" + HSSModules[i] + "\"></script>")
+    let script = document.createElement("script")
+    script.src = "HotSoupScript/Public/" + HSSModules[i]
+    document.body.appendChild(script)
 }
