29d9464c71003b818895d63f44ba6ebf53c5efb2
konbraphat51 <konbraphat@gmail.com>
2023-11-12 03:31:34 +0900

    Refac: reduce duiplication
    prettier ruins everything

--- Public/HTML.js
+++ Public/HTML.js
@@ -4,12 +4,12 @@
  * Erace all HTML elements made by this library.
  */
 function EraceAllHTML() {
-    for (let i = 0; i < __HSS_HTML_PRIVATE.elements_showing.length; i++) {
-        __HSS_HTML_PRIVATE.elements_showing[i].remove()
-    }
+	for (let i = 0; i < __HSS_HTML_PRIVATE.elements_showing.length; i++) {
+		__HSS_HTML_PRIVATE.elements_showing[i].remove()
+	}
 
-    //reset list
-    __HSS_HTML_PRIVATE.elements_showing = []
+	//reset list
+	__HSS_HTML_PRIVATE.elements_showing = []
 }
 
 /**
@@ -18,15 +18,15 @@ function EraceAllHTML() {
  * @param {string} id id of the HTML element
  */
 function EraceHTML(id) {
-    document.getElementById(id).remove()
+	document.getElementById(id).remove()
 
-    //remove from list
-    for (let i = 0; i < __HSS_HTML_PRIVATE.elements_showing.length; i++) {
-        if (__HSS_HTML_PRIVATE.elements_showing[i].id == id) {
-            __HSS_HTML_PRIVATE.elements_showing.splice(i, 1)
-            break
-        }
-    }
+	//remove from list
+	for (let i = 0; i < __HSS_HTML_PRIVATE.elements_showing.length; i++) {
+		if (__HSS_HTML_PRIVATE.elements_showing[i].id == id) {
+			__HSS_HTML_PRIVATE.elements_showing.splice(i, 1)
+			break
+		}
+	}
 }
 
 /**
@@ -43,37 +43,37 @@ function EraceHTML(id) {
  * @returns {string[]} [id of p tag, id of input tag]
  */
 function PutNumberInputField(
-    id,
-    label,
-    value,
-    min = -1e18,
-    max = 1e18,
-    step = 1,
-    onChanged = null
+	id,
+	label,
+	value,
+	min = -1e18,
+	max = 1e18,
+	step = 1,
+	onChanged = null,
 ) {
-    const input_id = MakeInputID(id)
+	const input_id = MakeInputID(id)
 
-    //document.write("<p id=\""+ id +"\">"+label+": <input type=\"number\" id=\"" + input_id + "\" value=\"" + value + "\" min=\"" + min + "\" max=\"" + max + "\" step=\"" + step + "\" onchange=\""+ onChanged.name +"()\"></p>")
-    const p = document.createElement("p")
-    p.id = id
-    const input = document.createElement("input")
-    input.type = "number"
-    input.id = input_id
-    input.value = value
-    input.min = min
-    input.max = max
-    input.step = step
-    if (onChanged != null) {
-        input.addEventListener("change", onChanged)
-    }
-    p.appendChild(document.createTextNode(label + ": "))
-    p.appendChild(input)
+	//document.write("<p id=\""+ id +"\">"+label+": <input type=\"number\" id=\"" + input_id + "\" value=\"" + value + "\" min=\"" + min + "\" max=\"" + max + "\" step=\"" + step + "\" onchange=\""+ onChanged.name +"()\"></p>")
+	const p = document.createElement("p")
+	p.id = id
+	const input = document.createElement("input")
+	input.type = "number"
+	input.id = input_id
+	input.value = value
+	input.min = min
+	input.max = max
+	input.step = step
+	if (onChanged != null) {
+		input.addEventListener("change", onChanged)
+	}
+	p.appendChild(document.createTextNode(label + ": "))
+	p.appendChild(input)
 
-    document.body.appendChild(p)
+	document.body.appendChild(p)
 
-    __HSS_HTML_PRIVATE.RegisterElementShowing(p)
+	__HSS_HTML_PRIVATE.RegisterElementShowing(p)
 
-    return [id, input_id]
+	return [id, input_id]
 }
 
 /**
@@ -88,45 +88,45 @@ function PutNumberInputField(
  * @returns {string[]} [id of p tag, id of input tag of mantissa, id of input tag of exponent]
  */
 function PutNumberInputFieldE(id, label, value, onChanged = null) {
-    const input_id_mantissa = MakeMantissaID(id)
-    const input_id_exponent = MakeExponentID(id)
+	const input_id_mantissa = MakeMantissaID(id)
+	const input_id_exponent = MakeExponentID(id)
 
-    //to E notation
-    let exponent = Math.floor(Math.log10(value))
-    let mantissa = value / 10 ** exponent
+	//to E notation
+	let exponent = Math.floor(Math.log10(value))
+	let mantissa = value / 10 ** exponent
 
-    //document.write("<p id=\"" + id + "\">" + label + ": <input type=\"number\" id=\"" + input_id_mantissa + "\" value=\"" + mantissa + "\" onchange=\"" + onChanged.name + "()\"> e <input type=\"number\" id=\"" + input_id_exponent + "\" value=\"" + exponent + "\" onchange=\"" + onChanged.name + "()\" step=\"1\"></p>")
-    const p = document.createElement("p")
-    p.id = id
-    const input_m = document.createElement("input")
-    input_m.type = "number"
-    input_m.id = input_id_mantissa
-    input_m.value = mantissa
-    if (onChanged != null) {
-        input_m.addEventListener("change", onChanged)
-    }
-    input_m.min = -10
-    input_m.max = 10
-    const input_e = document.createElement("input")
-    input_e.type = "number"
-    input_e.id = input_id_exponent
-    input_e.value = exponent
-    if (onChanged != null) {
-        input_e.addEventListener("change", onChanged)
-    }
-    input_e.min = -18
-    input_e.max = 18
-    input_e.step = 1
-    p.appendChild(document.createTextNode(label + ": "))
-    p.appendChild(input_m)
-    p.appendChild(document.createTextNode(" e "))
-    p.appendChild(input_e)
+	//document.write("<p id=\"" + id + "\">" + label + ": <input type=\"number\" id=\"" + input_id_mantissa + "\" value=\"" + mantissa + "\" onchange=\"" + onChanged.name + "()\"> e <input type=\"number\" id=\"" + input_id_exponent + "\" value=\"" + exponent + "\" onchange=\"" + onChanged.name + "()\" step=\"1\"></p>")
+	const p = document.createElement("p")
+	p.id = id
+	const input_m = document.createElement("input")
+	input_m.type = "number"
+	input_m.id = input_id_mantissa
+	input_m.value = mantissa
+	if (onChanged != null) {
+		input_m.addEventListener("change", onChanged)
+	}
+	input_m.min = -10
+	input_m.max = 10
+	const input_e = document.createElement("input")
+	input_e.type = "number"
+	input_e.id = input_id_exponent
+	input_e.value = exponent
+	if (onChanged != null) {
+		input_e.addEventListener("change", onChanged)
+	}
+	input_e.min = -18
+	input_e.max = 18
+	input_e.step = 1
+	p.appendChild(document.createTextNode(label + ": "))
+	p.appendChild(input_m)
+	p.appendChild(document.createTextNode(" e "))
+	p.appendChild(input_e)
 
-    document.body.appendChild(p)
+	document.body.appendChild(p)
 
-    __HSS_HTML_PRIVATE.RegisterElementShowing(p)
+	__HSS_HTML_PRIVATE.RegisterElementShowing(p)
 
-    return [id, input_id_mantissa, input_id_exponent]
+	return [id, input_id_mantissa, input_id_exponent]
 }
 
 /**
@@ -136,7 +136,7 @@ function PutNumberInputFieldE(id, label, value, onChanged = null) {
  * @return {string} Becareful that the return value is string. If you want number, use GetNumberInputFieldValue().
  */
 function GetInputFieldValue(id) {
-    return document.getElementById(id).value
+	return document.getElementById(id).value
 }
 
 /**
@@ -147,11 +147,11 @@ function GetInputFieldValue(id) {
  * @returns {number} number read.
  */
 function GetNumberInputFieldValue(id, fromPutFunc = false) {
-    if (fromPutFunc) {
-        id = MakeInputID(id)
-    }
+	if (fromPutFunc) {
+		id = MakeInputID(id)
+	}
 
-    return GetInputFieldValue(id) - 0
+	return GetInputFieldValue(id) - 0
 }
 
 /**
@@ -161,12 +161,12 @@ function GetNumberInputFieldValue(id, fromPutFunc = false) {
  * @returns {number} value calculated from the 2 input fields
  */
 function GetNumberInputFieldValueE(id) {
-    const id_mantissa = MakeMantissaID(id)
-    const id_exponent = MakeExponentID(id)
-    return (
-        GetNumberInputFieldValue(id_mantissa) *
-        10 ** GetNumberInputFieldValue(id_exponent)
-    )
+	const id_mantissa = MakeMantissaID(id)
+	const id_exponent = MakeExponentID(id)
+	return (
+		GetNumberInputFieldValue(id_mantissa) *
+		10 ** GetNumberInputFieldValue(id_exponent)
+	)
 }
 
 /**
@@ -177,7 +177,7 @@ function GetNumberInputFieldValueE(id) {
  * @returns {string} id of the input tag
  */
 function MakeInputID(id) {
-    return id + "_input"
+	return id + "_input"
 }
 
 /**
@@ -188,7 +188,7 @@ function MakeInputID(id) {
  * @returns {string} id of the input tag of mantissa
  */
 function MakeMantissaID(id) {
-    return id + "_input_m"
+	return id + "_input_m"
 }
 
 /**
@@ -199,7 +199,7 @@ function MakeMantissaID(id) {
  * @returns {string} id of the input tag of exponent
  */
 function MakeExponentID(id) {
-    return id + "_input_e"
+	return id + "_input_e"
 }
 
 /**
@@ -210,57 +210,57 @@ function MakeExponentID(id) {
  * @param {function} onClicked     Function called when clicked This will be multi-thread, so not recommended for beginners (Use IsHTMLUpdated() and GetHTMLButton())
  */
 function PutButton(id, label, onClicked = null) {
-    //document.write("<button id=\"" + id + "\" onclick=\"" + onClicked.name + "()\">" + label + "</button>")
-    const button = document.createElement("button")
-    button.id = id
-    if (onClicked != null) {
-        button.addEventListener("click", onClicked)
-    }
-    button.addEventListener(
-        "click",
-        __HSS_HTML_PRIVATE.StartListeningButton(button)
-    )
-    button.appendChild(document.createTextNode(label))
+	//document.write("<button id=\"" + id + "\" onclick=\"" + onClicked.name + "()\">" + label + "</button>")
+	const button = document.createElement("button")
+	button.id = id
+	if (onClicked != null) {
+		button.addEventListener("click", onClicked)
+	}
+	button.addEventListener(
+		"click",
+		__HSS_HTML_PRIVATE.StartListeningButton(button),
+	)
+	button.appendChild(document.createTextNode(label))
 
-    document.body.appendChild(button)
+	document.body.appendChild(button)
 
-    __HSS_HTML_PRIVATE.RegisterElementShowing(button)
+	__HSS_HTML_PRIVATE.RegisterElementShowing(button)
 }
 
 /**
  * Prevent default touch events like long-touch-selecting, scroling...
  */
 function StopAllTouchDefaults() {
-    const canvas = document.getElementById(__CANVAS_NAME)
+	const canvas = document.getElementById(__CANVAS_NAME)
 
-    canvas.addEventListener(
-        "touchstart",
-        function (e) {
-            e.preventDefault()
-        },
-        { passive: false }
-    )
-    canvas.addEventListener(
-        "touchmove",
-        function (e) {
-            e.preventDefault()
-        },
-        { passive: false }
-    )
-    canvas.addEventListener(
-        "touchend",
-        function (e) {
-            e.preventDefault()
-        },
-        { passive: false }
-    )
-    canvas.addEventListener(
-        "touchcancel",
-        function (e) {
-            e.preventDefault()
-        },
-        { passive: false }
-    )
+	canvas.addEventListener(
+		"touchstart",
+		function (e) {
+			e.preventDefault()
+		},
+		{passive: false},
+	)
+	canvas.addEventListener(
+		"touchmove",
+		function (e) {
+			e.preventDefault()
+		},
+		{passive: false},
+	)
+	canvas.addEventListener(
+		"touchend",
+		function (e) {
+			e.preventDefault()
+		},
+		{passive: false},
+	)
+	canvas.addEventListener(
+		"touchcancel",
+		function (e) {
+			e.preventDefault()
+		},
+		{passive: false},
+	)
 }
 
 /**
@@ -270,12 +270,12 @@ function StopAllTouchDefaults() {
  * @returns {string[]} IDs of updated HTML elements
  */
 function GetUpdatedHTMLs(should_reset_list = true) {
-    const updated = CopyArray(__HSS_HTML_PRIVATE.updated_inputs)
-    if (should_reset_list) {
-        __HSS_HTML_PRIVATE.updated_inputs = []
-    }
+	const updated = CopyArray(__HSS_HTML_PRIVATE.updated_inputs)
+	if (should_reset_list) {
+		__HSS_HTML_PRIVATE.updated_inputs = []
+	}
 
-    return updated
+	return updated
 }
 
 /**
@@ -286,15 +286,15 @@ function GetUpdatedHTMLs(should_reset_list = true) {
  * @returns {boolean} Wheather the element is updated by user input.
  */
 function IsHTMLUpdated(id, should_remove = true) {
-    const updated = __HSS_HTML_PRIVATE.updated_inputs.includes(id)
-    if (updated && should_remove) {
-        __HSS_HTML_PRIVATE.updated_inputs.splice(
-            __HSS_HTML_PRIVATE.updated_inputs.indexOf(id),
-            1
-        )
-    }
+	const updated = __HSS_HTML_PRIVATE.updated_inputs.includes(id)
+	if (updated && should_remove) {
+		__HSS_HTML_PRIVATE.updated_inputs.splice(
+			__HSS_HTML_PRIVATE.updated_inputs.indexOf(id),
+			1,
+		)
+	}
 
-    return updated
+	return updated
 }
 
 /**
@@ -303,12 +303,12 @@ function IsHTMLUpdated(id, should_remove = true) {
  * @param {string} id   id of the HTML element to remove
  */
 function RemoveHTMLUpdateList(id) {
-    if (__HSS_HTML_PRIVATE.updated_inputs.includes(id)) {
-        __HSS_HTML_PRIVATE.updated_inputs.splice(
-            __HSS_HTML_PRIVATE.updated_inputs.indexOf(id),
-            1
-        )
-    }
+	if (__HSS_HTML_PRIVATE.updated_inputs.includes(id)) {
+		__HSS_HTML_PRIVATE.updated_inputs.splice(
+			__HSS_HTML_PRIVATE.updated_inputs.indexOf(id),
+			1,
+		)
+	}
 }
 
 /**
@@ -317,11 +317,11 @@ function RemoveHTMLUpdateList(id) {
  * @param {string} id   HTML id of the button
  */
 function StartListenButton(id) {
-    const button = document.getElementById(id)
-    button.addEventListener(
-        "click",
-        __HSS_HTML_PRIVATE.StartListeningButton(button)
-    )
+	const button = document.getElementById(id)
+	button.addEventListener(
+		"click",
+		__HSS_HTML_PRIVATE.StartListeningButton(button),
+	)
 }
 
 //--------------------Not for users--------------------
@@ -330,38 +330,42 @@ function StartListenButton(id) {
  * Not for using.
  */
 class __HSS_HTML_Private {
-    //elements showed by this library part.
-    elements_showing = []
+	//elements showed by this library part.
+	elements_showing = []
 
-    //IDs of html tag updated by user input
-    updated_inputs = []
+	//IDs of html tag updated by user input
+	updated_inputs = []
 
-    RegisterElementShowing(tag) {
-        this.elements_showing.push(tag)
-    }
+	RegisterElementShowing(tag) {
+		this.elements_showing.push(tag)
+	}
 
-    StartListeningChange() {
-        //listen to input
-        document.addEventListener("change", function (e) {
-            //prevent duplication
-            if (__HSS_HTML_PRIVATE.updated_inputs.includes(e.target.id)) {
-                return
-            }
+	StartListeningChange() {
+		//listen to input
+		document.addEventListener("change", function (e) {
+			//prevent duplication
+			if (__HSS_HTML_PRIVATE.updated_inputs.includes(e.target.id)) {
+				return
+			}
 
-            __HSS_HTML_PRIVATE.updated_inputs.push(e.target.id)
-        })
-    }
+			__HSS_HTML_PRIVATE.updated_inputs.push(e.target.id)
+		})
+	}
 
-    StartListeningButton(element) {
-        element.addEventListener("click", function (e) {
-            //prevent duplication
-            if (__HSS_HTML_PRIVATE.updated_inputs.includes(e.target.id)) {
-                return
-            }
+	StartListeningButton(element) {
+		this._StartListening("click")
+	}
 
-            __HSS_HTML_PRIVATE.updated_inputs.push(e.target.id)
-        })
-    }
+	_StartListening(event_name) {
+		document.addEventListener(event_name, function (e) {
+			//prevent duplication
+			if (__HSS_HTML_PRIVATE.updated_inputs.includes(e.target.id)) {
+				return
+			}
+
+			__HSS_HTML_PRIVATE.updated_inputs.push(e.target.id)
+		})
+	}
 }
 
 const __HSS_HTML_PRIVATE = new __HSS_HTML_Private()
